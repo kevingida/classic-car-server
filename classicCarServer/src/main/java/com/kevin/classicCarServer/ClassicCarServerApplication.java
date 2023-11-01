11ea483a8b33fa27f09c6175c7def5c7182a35f2
Kevin Gida <139972073+KevinGida@users.noreply.github.com>
2023-11-01 08:53:23 +0100

    Add server

---
+++ classicCarServer/src/main/java/com/kevin/classicCarServer/ClassicCarServerApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.classicCarServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicCarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicCarServerApplication.class, args);
+	}
+
+}
---
+++ classicCarServer/src/test/java/com/kevin/classicCarServer/ClassicCarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.classicCarServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicCarServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
