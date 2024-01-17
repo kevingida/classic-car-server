db72970cb73fcfb12ce2d3b9a7595e13302237f0
Kevin Gida <kevingida@gmail.com>
2024-01-17 20:38:39 +0100

    add findbyName to BodyTypeRepository

--- classicCarServer/src/main/java/com/kevin/classicCarServer/car/repository/BodyTypeRepository.java
+++ classicCarServer/src/main/java/com/kevin/classicCarServer/car/repository/BodyTypeRepository.java
@@ -1,8 +1,8 @@
 package com.kevin.classicCarServer.car.repository;
 
 import com.kevin.classicCarServer.car.models.BodyType;
-import com.kevin.classicCarServer.car.models.Car;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface BodyTypeRepository extends ListCrudRepository<BodyType, String> {
+    BodyType findByName(String name);
 }
